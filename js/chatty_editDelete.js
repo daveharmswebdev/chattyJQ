4ec912ee2d5086db3035001cbe91630ca700e200
Dave Harms <daveharmswebdev@gmail.com>
2016-06-16 10:32:33 -0500

    able to edit and add new message objects and render new objects

--- js/chatty_editDelete.js
+++
@@ -1,8 +0,0 @@
-var chatty = (function($, chat) {
-
-	chat.editMessage = () => console.log('edit');
-	chat.deleteMessage = () => console.log('delete');
-
-	return chat;
-
-})(jQuery, chatty || {});
--- js/chatty_userMessage.js
+++ js/chatty_userMessage.js
@@ -2,22 +2,69 @@ var chatty = (function($, chat) {
 
     // private variables
     var messageCounter,
-    	elMessagesDiv = $('#messagesDiv'),
+    	$elMessagesDiv = $('#messagesDiv'),
+    	$elMessageInput = $('#messageInput'),
+    	idToEdit,
+    	editEnabled = false;
     	users = [],
     	messages = [],
     	archive = [];
 
-    elMessagesDiv.on('click', '.btnDelete', function(event) {
+    $elMessagesDiv.on('click', '.btnDelete', function(event) {
     	var idToDelete = parseInt(event.target.id.split('--')[1]);
     	event.target.closest('.message').remove();
     	chat.removeFromMessages(idToDelete);
     });
 
+    $elMessagesDiv.on('click', '.btnEdit', function(event) {
+    	editEnabled = true;
+    	idToEdit = parseInt(event.target.id.split('--')[1]);
+    	$elMessageInput.val($(`#content--${idToEdit}`).text().split('posted@')[0]);
+    	$elMessageInput.focus();
+    });
+
+    $elMessageInput.on('keydown', function(event) {
+    	if (event.keyCode === 13) {
+			let editTime = new Date().toUTCString();
+    		if (editEnabled === true) {
+    			let newMessage = $elMessageInput.val() + ' posted@ ' + editTime + '(edit)';
+    			let indexToEdit = getIndex(idToEdit);
+    			messages[indexToEdit].message = newMessage;
+    			$(`#content--${idToEdit}`).text(newMessage);
+    			editEnabled = false;
+    		} else {
+    			messages.push(getMessageObj($elMessageInput.val()));
+    			let x = messages.length - 1;
+    			chatty.renderMessage(
+    				messages[x].user,
+    				messages[x].message,
+    				messages[x].time,
+    				messages[x].id
+    				);
+    		}
+    	$elMessageInput.val('');
+    	}
+    });
+
+    function getIndex(id) {
+    	return messages.findIndex((message) => message.id === id);
+    }
+
+    function getMessageObj(message) {
+    	messageCounter++
+    	var obj = {};
+		obj.id = messageCounter;
+		obj.user = $('#userSelect').val();
+		obj.message = message;
+		obj.time = new Date().toUTCString();
+		return obj;
+    }
+
     chat.renderMessage = function(user, message, time, id) {
         var elMessDiv = $('<div/>').attr({
             id: `message--${id}`,
             class: 'message'
-        }).appendTo(elMessagesDiv);
+        }).appendTo($elMessagesDiv);
         var elPUser = $('<p/>').attr({
             id: `user--${id}`,
             class: 'user'
@@ -25,10 +72,7 @@ var chatty = (function($, chat) {
         var elPMessage = $('<p/>').attr({
             id: `content--${id}`,
             class: 'content'
-        }).text(message).appendTo(elMessDiv);
-        var elSpanTime = $('<span/>').attr({
-            class: 'time'
-        }).text(time).appendTo(elPMessage);
+        }).text(`${message} posted@ ${time}`).appendTo(elMessDiv);
         var elBtnDelete = $('<button/>;').attr({
         	id: `delete--${id}`,
         	class: 'btnDelete'
